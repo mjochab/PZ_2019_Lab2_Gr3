46d9b5148cedfd4ca88ff8bf9a1d7b88042b6584
Matyaszek <35657547+Matyaszek@users.noreply.github.com>
2019-05-13 12:50:49 +0200

    Przebudowa bazy danych

--- database/przychodnia.sql
+++ database/przychodnia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 15 Kwi 2019, 18:00
+-- Czas generowania: 13 Maj 2019, 12:10
 -- Wersja serwera: 10.1.38-MariaDB
--- Wersja PHP: 7.3.2
+-- Wersja PHP: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,71 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `przychodnia`
 --
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `artykuly`
---
-
-CREATE TABLE `artykuly` (
-  `artykul_id` int(11) NOT NULL,
-  `tytul` varchar(200) COLLATE utf8_polish_ci NOT NULL,
-  `tresc` text COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `choroby`
---
-
-CREATE TABLE `choroby` (
-  `choroba_id` int(3) NOT NULL,
-  `nazwa` varchar(30) COLLATE utf8_polish_ci NOT NULL,
-  `opis` text COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `konta`
---
-
-CREATE TABLE `konta` (
-  `id` int(4) NOT NULL,
-  `imie` varchar(20) NOT NULL,
-  `nazwisko` varchar(30) NOT NULL,
-  `login` varchar(50) DEFAULT NULL,
-  `haslo` varchar(50) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `nr_tel` int(11) NOT NULL,
-  `PESEL` int(11) NOT NULL,
-  `miejscowosc` varchar(30) DEFAULT NULL,
-  `ulica` varchar(30) DEFAULT NULL,
-  `nr_domu` int(3) DEFAULT NULL,
-  `nr_lokalu` int(3) DEFAULT NULL,
-  `kod_pocztowy` varchar(6) DEFAULT NULL,
-  `poczta` varchar(30) DEFAULT NULL,
-  `typ_konta` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `recepty`
---
-
-CREATE TABLE `recepty` (
-  `ID_Recepty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Zrzut danych tabeli `recepty`
---
-
-INSERT INTO `recepty` (`ID_Recepty`) VALUES
-(451236);
 
 -- --------------------------------------------------------
 
@@ -107,18 +42,28 @@ CREATE TABLE `wizyty` (
 --
 
 INSERT INTO `wizyty` (`Data_wizyty`, `Czas_wizyty`, `ID_Wizyta`, `ID_Lekarz`, `ID_Pacjent`, `ID_Recepty`) VALUES
-('0000-00-00 00:00:00', '00:00:00', 547895, 4568789, 541232, 111111),
-('2019-04-01 12:00:00', '00:20:00', 234562, 132445, 231232, 321432);
+('2019-04-01 12:00:00', '00:20:00', 234562, 132445, 231232, 321432),
+('0000-00-00 00:00:00', '00:00:00', 547895, 4568789, 541232, 111111);
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indeksy dla tabeli `recepty`
+-- Indeksy dla tabeli `wizyty`
 --
-ALTER TABLE `recepty`
-  ADD PRIMARY KEY (`ID_Recepty`);
+ALTER TABLE `wizyty`
+  ADD PRIMARY KEY (`ID_Wizyta`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `wizyty`
+--
+ALTER TABLE `wizyty`
+  MODIFY `ID_Wizyta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=547896;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
