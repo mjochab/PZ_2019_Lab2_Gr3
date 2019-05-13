758a0dc80c5269791bfcb70d8ad6287b1e995d79
Matyaszek <35657547+Matyaszek@users.noreply.github.com>
2019-05-13 13:50:53 +0200

    Update przychodnia.sql

--- database/przychodnia.sql
+++ database/przychodnia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 13 Maj 2019, 13:16
--- Wersja serwera: 10.1.38-MariaDB
--- Wersja PHP: 7.3.4
+-- Czas generowania: 13 Maj 2019, 13:28
+-- Wersja serwera: 10.1.36-MariaDB
+-- Wersja PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,8 +31,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `artykuly` (
   `artykul_id` int(11) NOT NULL,
   `tytul` varchar(200) COLLATE utf8_polish_ci NOT NULL,
-  `tresc` text COLLATE utf8_polish_ci NOT NULL
+  `tresc` text COLLATE utf8_polish_ci NOT NULL,
+  `data` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `artykuly`
+--
+
+INSERT INTO `artykuly` (`artykul_id`, `tytul`, `tresc`, `data`) VALUES
+(1, 'BEZPLATNE BADANIE SLUCHU', 'tresc artukulu', '2019-04-15');
 
 -- --------------------------------------------------------
 
@@ -46,6 +54,13 @@ CREATE TABLE `choroby` (
   `opis` text COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+--
+-- Zrzut danych tabeli `choroby`
+--
+
+INSERT INTO `choroby` (`choroba_id`, `nazwa`, `opis`) VALUES
+(1, 'Zapalenie pluc', 'Zapalenie pluc njkfnjdsnj fndjksfnd nfdjs nfdsfn jdsnfkdns jkdsnj snjfkndjsfkdsnfk');
+
 -- --------------------------------------------------------
 
 --
@@ -53,30 +68,106 @@ CREATE TABLE `choroby` (
 --
 
 CREATE TABLE `konta` (
-  `id` int(4) NOT NULL,
-  `imie` varchar(20) NOT NULL,
-  `nazwisko` varchar(30) NOT NULL,
-  `login` varchar(50) DEFAULT NULL,
-  `haslo` varchar(50) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `nr_tel` varchar(11) NOT NULL,
-  `PESEL` varchar(11) NOT NULL,
-  `miejscowosc` varchar(30) DEFAULT NULL,
-  `ulica` varchar(30) DEFAULT NULL,
-  `nr_domu` varchar(3) DEFAULT NULL,
-  `nr_lokalu` varchar(3) DEFAULT NULL,
-  `kod_pocztowy` varchar(6) DEFAULT NULL,
-  `poczta` varchar(30) DEFAULT NULL,
-  `typ_konta` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `konto_id` int(4) NOT NULL,
+  `login` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `haslo` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `rodzaj_konta` varchar(13) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Zrzut danych tabeli `konta`
 --
 
-INSERT INTO `konta` (`id`, `imie`, `nazwisko`, `login`, `haslo`, `email`, `nr_tel`, `PESEL`, `miejscowosc`, `ulica`, `nr_domu`, `nr_lokalu`, `kod_pocztowy`, `poczta`, `typ_konta`) VALUES
-(2, 'Jan', 'Kowalski', 'login', 'haslo', NULL, '', '', NULL, NULL, NULL, NULL, NULL, NULL, 'pacjenci'),
-(3, 'Marek', 'Nowak', 'login2', 'haslo', NULL, '', '', NULL, NULL, NULL, NULL, NULL, NULL, 'lekarze');
+INSERT INTO `konta` (`konto_id`, `login`, `haslo`, `rodzaj_konta`) VALUES
+(1, 'jkowalski32', 'kowal123', 'pacjenci'),
+(5, 'anowak', '1234', 'lekarze'),
+(6, 'kkrol', '1234', 'recepcjonisci');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `lekarze`
+--
+
+CREATE TABLE `lekarze` (
+  `lekarz_id` int(4) NOT NULL,
+  `imie` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_polish_ci DEFAULT NULL,
+  `nr_tel` int(11) NOT NULL,
+  `PESEL` int(11) NOT NULL,
+  `miejscowosc` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
+  `ulica` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
+  `nr_domu` int(3) DEFAULT NULL,
+  `nr_lokalu` int(3) DEFAULT NULL,
+  `kod_pocztowy` varchar(6) COLLATE utf8_polish_ci DEFAULT NULL,
+  `poczta` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
+  `konto_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `leki`
+--
+
+CREATE TABLE `leki` (
+  `lek_id` int(4) NOT NULL,
+  `nazwa` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `opis` text COLLATE utf8_polish_ci NOT NULL,
+  `dawkowanie` varchar(100) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pacjenci`
+--
+
+CREATE TABLE `pacjenci` (
+  `pacjent_id` int(4) NOT NULL,
+  `imie` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `nr_tel` int(11) NOT NULL,
+  `PESEL` bigint(11) NOT NULL,
+  `miejscowosc` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `ulica` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `nr_domu` int(3) NOT NULL,
+  `nr_lokalu` int(3) NOT NULL,
+  `kod_pocztowy` varchar(6) COLLATE utf8_polish_ci NOT NULL,
+  `poczta` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `konto_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `pacjenci`
+--
+
+INSERT INTO `pacjenci` (`pacjent_id`, `imie`, `nazwisko`, `email`, `nr_tel`, `PESEL`, `miejscowosc`, `ulica`, `nr_domu`, `nr_lokalu`, `kod_pocztowy`, `poczta`, `konto_id`) VALUES
+(1, 'Jan', 'Kowalski', 'jkowalski32@gmail.com', 166216905, 87031787987, 'Rzeszow', 'Tadeusza Rejtana', 16, 5, '37-119', 'Rzeszow', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `recepcjonisci`
+--
+
+CREATE TABLE `recepcjonisci` (
+  `recepcjonista_id` int(4) NOT NULL,
+  `imie` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
+  `nr_tel` int(11) NOT NULL,
+  `PESEL` int(11) NOT NULL,
+  `miejscowosc` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
+  `ulica` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
+  `nr_domu` int(3) DEFAULT NULL,
+  `nr_lokalu` int(3) DEFAULT NULL,
+  `kod_pocztowy` varchar(6) COLLATE utf8_polish_ci DEFAULT NULL,
+  `poczta` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
+  `konto_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 -- --------------------------------------------------------
 
@@ -85,15 +176,11 @@ INSERT INTO `konta` (`id`, `imie`, `nazwisko`, `login`, `haslo`, `email`, `nr_te
 --
 
 CREATE TABLE `recepty` (
-  `ID_Recepty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Zrzut danych tabeli `recepty`
---
-
-INSERT INTO `recepty` (`ID_Recepty`) VALUES
-(451236);
+  `recepta_id` int(11) NOT NULL,
+  `data_wystawienia` date NOT NULL,
+  `lek_id` int(11) NOT NULL,
+  `uwagi` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 -- --------------------------------------------------------
 
@@ -102,21 +189,15 @@ INSERT INTO `recepty` (`ID_Recepty`) VALUES
 --
 
 CREATE TABLE `wizyty` (
-  `Data_wizyty` datetime NOT NULL,
-  `Czas_wizyty` time NOT NULL,
-  `ID_Wizyta` int(11) NOT NULL,
-  `ID_Lekarz` int(11) NOT NULL,
-  `ID_Pacjent` int(11) NOT NULL,
-  `ID_Recepty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Zrzut danych tabeli `wizyty`
---
-
-INSERT INTO `wizyty` (`Data_wizyty`, `Czas_wizyty`, `ID_Wizyta`, `ID_Lekarz`, `ID_Pacjent`, `ID_Recepty`) VALUES
-('2019-04-01 12:00:00', '00:20:00', 234562, 132445, 231232, 321432),
-('0000-00-00 00:00:00', '00:00:00', 547895, 4568789, 541232, 111111);
+  `wizyta_id` int(5) NOT NULL,
+  `recepcjonista_id` int(3) NOT NULL,
+  `lekarz_id` int(3) NOT NULL,
+  `pacjent_id` int(4) NOT NULL,
+  `recepta_id` int(5) NOT NULL,
+  `choroba_id` int(4) DEFAULT NULL,
+  `opis` text COLLATE utf8_polish_ci NOT NULL,
+  `data` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Indeksy dla zrzutów tabel
@@ -138,19 +219,52 @@ ALTER TABLE `choroby`
 -- Indeksy dla tabeli `konta`
 --
 ALTER TABLE `konta`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`konto_id`);
+
+--
+-- Indeksy dla tabeli `lekarze`
+--
+ALTER TABLE `lekarze`
+  ADD PRIMARY KEY (`lekarz_id`),
+  ADD KEY `konto_id` (`konto_id`);
+
+--
+-- Indeksy dla tabeli `leki`
+--
+ALTER TABLE `leki`
+  ADD PRIMARY KEY (`lek_id`);
+
+--
+-- Indeksy dla tabeli `pacjenci`
+--
+ALTER TABLE `pacjenci`
+  ADD PRIMARY KEY (`pacjent_id`),
+  ADD KEY `konto_id` (`konto_id`);
+
+--
+-- Indeksy dla tabeli `recepcjonisci`
+--
+ALTER TABLE `recepcjonisci`
+  ADD PRIMARY KEY (`recepcjonista_id`),
+  ADD KEY `konto_id` (`konto_id`);
 
 --
 -- Indeksy dla tabeli `recepty`
 --
 ALTER TABLE `recepty`
-  ADD PRIMARY KEY (`ID_Recepty`);
+  ADD PRIMARY KEY (`recepta_id`),
+  ADD KEY `id_leku` (`lek_id`);
 
 --
 -- Indeksy dla tabeli `wizyty`
 --
 ALTER TABLE `wizyty`
-  ADD PRIMARY KEY (`ID_Wizyta`);
+  ADD PRIMARY KEY (`wizyta_id`),
+  ADD KEY `pacjent_id` (`pacjent_id`),
+  ADD KEY `recepcjonista_id` (`recepcjonista_id`),
+  ADD KEY `lekarz_id` (`lekarz_id`),
+  ADD KEY `recepta_id` (`recepta_id`),
+  ADD KEY `choroba_id` (`choroba_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -160,25 +274,93 @@ ALTER TABLE `wizyty`
 -- AUTO_INCREMENT dla tabeli `artykuly`
 --
 ALTER TABLE `artykuly`
-  MODIFY `artykul_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `artykul_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `choroby`
 --
 ALTER TABLE `choroby`
-  MODIFY `choroba_id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `choroba_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `konta`
 --
 ALTER TABLE `konta`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `konto_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT dla tabeli `lekarze`
+--
+ALTER TABLE `lekarze`
+  MODIFY `lekarz_id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `leki`
+--
+ALTER TABLE `leki`
+  MODIFY `lek_id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `pacjenci`
+--
+ALTER TABLE `pacjenci`
+  MODIFY `pacjent_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `recepcjonisci`
+--
+ALTER TABLE `recepcjonisci`
+  MODIFY `recepcjonista_id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `recepty`
+--
+ALTER TABLE `recepty`
+  MODIFY `recepta_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT dla tabeli `wizyty`
 --
 ALTER TABLE `wizyty`
-  MODIFY `ID_Wizyta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=547896;
+  MODIFY `wizyta_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `lekarze`
+--
+ALTER TABLE `lekarze`
+  ADD CONSTRAINT `lekarze_ibfk_1` FOREIGN KEY (`lekarz_id`) REFERENCES `wizyty` (`lekarz_id`),
+  ADD CONSTRAINT `lekarze_ibfk_2` FOREIGN KEY (`konto_id`) REFERENCES `konta` (`konto_id`);
+
+--
+-- Ograniczenia dla tabeli `pacjenci`
+--
+ALTER TABLE `pacjenci`
+  ADD CONSTRAINT `pacjenci_ibfk_1` FOREIGN KEY (`konto_id`) REFERENCES `konta` (`konto_id`);
+
+--
+-- Ograniczenia dla tabeli `recepcjonisci`
+--
+ALTER TABLE `recepcjonisci`
+  ADD CONSTRAINT `recepcjonisci_ibfk_1` FOREIGN KEY (`konto_id`) REFERENCES `konta` (`konto_id`);
+
+--
+-- Ograniczenia dla tabeli `recepty`
+--
+ALTER TABLE `recepty`
+  ADD CONSTRAINT `recepty_ibfk_1` FOREIGN KEY (`lek_id`) REFERENCES `leki` (`lek_id`);
+
+--
+-- Ograniczenia dla tabeli `wizyty`
+--
+ALTER TABLE `wizyty`
+  ADD CONSTRAINT `wizyty_ibfk_1` FOREIGN KEY (`pacjent_id`) REFERENCES `pacjenci` (`pacjent_id`),
+  ADD CONSTRAINT `wizyty_ibfk_2` FOREIGN KEY (`recepcjonista_id`) REFERENCES `recepcjonisci` (`recepcjonista_id`),
+  ADD CONSTRAINT `wizyty_ibfk_3` FOREIGN KEY (`choroba_id`) REFERENCES `choroby` (`choroba_id`),
+  ADD CONSTRAINT `wizyty_ibfk_4` FOREIGN KEY (`recepta_id`) REFERENCES `recepty` (`recepta_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
