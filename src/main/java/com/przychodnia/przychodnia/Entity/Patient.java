e0ba7ffc98dece93262dbdf195bea64312f5d8fd
KrzysztofPieta <Krzysztofpieta1997@gmail.com>
2019-05-16 16:54:07 +0200

    klasy entity i mapowanie hibernate

---
+++ src/main/java/com/przychodnia/przychodnia/Entity/Patient.java
@@ -0,0 +1,100 @@
+package com.przychodnia.przychodnia.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "p_patient")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String password;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String pesel;
+
+    private String tel_number;
+
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getTel_number() {
+        return tel_number;
+    }
+
+    public void setTel_number(String tel_number) {
+        this.tel_number = tel_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/com/przychodnia/przychodnia/Entity/Receptionist.java
@@ -0,0 +1,70 @@
+package com.przychodnia.przychodnia.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "p_receptionist")
+public class Receptionist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String password;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
